69c7956f3396afd311cd117ccadf76520d67947f
Najma Saeed <najmasy20@gmail.com>
2023-11-05 20:00:44 +0000

    need to add weather info

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const data = require("./data/weather.json");
 
 app.get("/", (_, response) => response.json("Root route."));
 
-app.get("/weather", (request, response) => {
+app.get("/weather", async (request, response) => {
   const { lat, lon, searchQuery } = request.query;
-
-  // check if searchQuery,lat and lon match json file - filter
 
   //  `http://localhost:8080/weather?lat=${lat}&lon=${lon}&searchQuery=${searchQuery}`;
 
+  // filter cities based on searchQuery, lat, lon
   const filteredCity = data.find((city) => {
-    return city.city_name === searchQuery && city.lat == lat && city.lon == lon;
+    return city.city_name === searchQuery;
+    // && city.lat == lat && city.lon == lon;
   });
 
   const wrangledData = filteredCity.data.map((day) => {
